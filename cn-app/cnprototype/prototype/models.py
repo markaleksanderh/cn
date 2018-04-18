32319030b2b2a3fa4d1aafdbbaeed42c16809939
markaleksanderh <mhil01@dcs.bbk.ac.uk>
2018-04-18 13:55:48 +0100

    Custom user foreign key error.

--- cn-app/cnprototype/prototype/models.py
+++ cn-app/cnprototype/prototype/models.py
@@ -1,23 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 from django.forms import ModelForm
 
-
-# Create your models here.
-
-"""
-LIST MODELS
--- Job
--- Company
-
-???
--- Region
--- Job type
-"""
-
 class CustomUser(AbstractUser):
-    # First/last name is not a global-friendly pattern
     name = models.CharField(blank=True, max_length=255)
     first_name = models.CharField(max_length=100, blank=True)
     second_name = models.CharField(max_length=100, blank=True)
@@ -27,12 +14,12 @@ class CustomUser(AbstractUser):
 
 class Job(models.Model):
     name = models.CharField(max_length=200, help_text="Enter a job name")
-    description = models.CharField(max_length=1000, help_text="Enter job description", null=True, blank=True)
+    description = models.TextField(max_length=750, help_text="Enter job description", null=True, blank=True)
     value = models.IntegerField(null=True, blank=True, help_text="Enter job value")
     company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
     added = models.DateTimeField(auto_now_add=True)
     region = models.ForeignKey('Region', on_delete=models.SET_NULL, null=True)
-    # added_by = models.ForeignKey('CustomUser', on_delete=models.SET_NULL, null=True)
+    added_by = models.ForeignKey('settings.AUTH_USER_MODEL', on_delete=models.CASCADE)
     def __str__(self):
         return self.name
 
