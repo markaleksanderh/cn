122fddbd55454c33d59cfaa169938566778de260
markaleksanderh <mhil01@dcs.bbk.ac.uk>
2018-04-27 12:42:42 +0100

    removed comments

--- lgc-scrape/scr2.py
+++ lgc-scrape/scr2.py
@@ -1,21 +1,3 @@
-# from bs4 import BeautifulSoup
-#
-# import requests
-# import urllib
-#
-# url = ("https://www.gov.uk/government/collections/workforce-management")
-#
-# r  = requests.get(url)
-#
-# ##data = url.text
-#
-# soup = BeautifulSoup(url)
-# link = soup.find('a', href=True)
-#
-# for link in soup.find_all("a"):
-# 	print(link.get('href'))
-
-
 import requests
 from bs4 import BeautifulSoup
 
@@ -30,8 +12,10 @@ gov_soup = BeautifulSoup(gov_req, 'html.parser')
 
 # Anchor tags don't have classes so select parent element then select anchor child element after
 # First, find all h3 tags with class 'gem-c-document-list__item-title'
+# This grabs only the relevant h3 tags
 h3_soup = gov_soup.findAll('h3', {'class': 'gem-c-document-list__item-title'})
 
+# Now loop through array of h3 elements
 for a in h3_soup:
 	print(a.find('a'))
 
@@ -39,5 +23,3 @@ for a in h3_soup:
 # href attribute of anchor tags is relative so to get the scraper to work,
 # be sure to provide the rest of the url
 base_url = ''
-#
-# print(child_anchor_element)
